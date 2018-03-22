ebcbcb0f14c925d57e8bb09f4780eb51ae2fbcd3
HatsuneBlake <374547020@qq.com>
2018-03-22 21:18:39 +0800

    ex2

--- ex322-2.py
+++ ex322-2.py
@@ -1,19 +1,21 @@
-class A3(object):
+class A4(object):
 	phonelen = 11
-	retireage = 60
+	retireage = 59
+	
 	def __init__(self,name,age,phone,addr):
 		self.name = name
 		self.age = age
 		self.phone = phone
 		self.addr = addr
 		
-	def __getattr__(self,attr):
+	def __getattribute__(self,attr):
+		superget = object.__getattribute__
 		if attr == 'phone':
-			return self._phone[:-3] + '***'
+			return superget(self,'phone')[:-3] + '***'
 		if attr == 'remain':
-			return (self.retireage - self.age)
+			return superget(self,'retireage') - superget(self,'age')
 		else:
-			raise AttributeError('Not Exist')
+			return superget(self,attr)
 		
 	def __setattr__(self,attr,value):
 		if attr == 'age':
@@ -21,16 +23,16 @@ class A3(object):
 				raise ValueError('Invalid age')
 		if attr == 'phone':
 			value = str(value)
-			attr = '_phone'
 			if len(value) != self.phonelen:
-				raise AttributeError('Invalid phone number')
-		if attr == 'remain':
+				raise ValueError('Invalid phone number')
+		if attr == 'remian':
 			raise TypeError('Cannot set')
 		self.__dict__[attr] = value
 		
-
+		
+		
 if __name__ == '__main__':
-	s = A3('Sue Jones',18,12345678901,'Area3')
+	s = A4('Sue Jones',18,12345678901,'Area3')
 	print s.phone
 	print s.remain
 	
